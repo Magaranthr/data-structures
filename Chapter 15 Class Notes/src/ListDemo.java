264c99c3329c1388698b1c75309486e2c3a09b8a
Magaranthr <mrajkumar@stu.naperville203.org>
2025-08-25 13:17:25 -0500

    day 1
    forgot my airpods somewher eper usual
    going to look fro them now
    how to tell borge hes gonna be MAD

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -9,6 +9,25 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+    LinkedList<String> staff = new LinkedList<>();
+    
+        staff.addLast("Tony");
+        staff.addLast("Natasha");
+        staff.addLast("Peter");
+
+        staff.addFirst("Dchollilia");
+        System.out.println(staff);
         
+        ListIterator<String> iterator = staff.listIterator();
+        iterator.next();
+        iterator.add("Steve");
+        System.out.println(staff);
+    
+    
+    
+    
+    
+    
+    
     }
 }
