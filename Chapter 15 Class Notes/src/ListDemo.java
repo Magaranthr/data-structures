0fed3210480ffad0d6d016596dceb583eecc5e12
Magaranthr <mrajkumar@stu.naperville203.org>
2025-08-26 13:18:10 -0500

    DAY 2
    day 1.5 edit
    actually they were in my bad, i sw them moing and got confused but it turns out im really smart nad put em in my bag
    
    day 2
    um so tchalla was finally addded o the start also it turns vscode want me to code slurs. it even said a really bad word for tchalla

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -20,14 +20,45 @@ public class ListDemo
         
         ListIterator<String> iterator = staff.listIterator();
         iterator.next();
-        iterator.add("Steve");
+        iterator.add("notasamurai");
+        System.out.println(staff);
+        iterator.next();
+        iterator.remove();
         System.out.println(staff);
     
+        // examples of hasNext and hasPrevious go here
+        System.out.println("Iterating forward:");  
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
     
-    
-    
-    
-    
-    
+        //what i should do to cheat on the sat is study nut i wont so what do i do
+        System.out.println("Iterating backward:");
+    // the famous famous quote by lebron james is 
+        while (iterator.hasPrevious()) {
+            String name = iterator.previous();
+            System.out.println(name);
+        }
+        // the most famous quote by lebron james is "i am king"
+        //reset the iterator to the start of the list
+        iterator = staff.listIterator();
+        //make a while loop with has next and go next to print all names
+        System.out.println("Resetting iterator and printing names:");
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }  
+        //but remove tony from the list
+        iterator = staff.listIterator();
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            if (name.equals("Tony")) {
+                iterator.remove();
+            }
+        }
+        // use the import to getinformation from a link on the internet
+        System.out.println("After removing Tony:");
+        System.out.println(staff);
     }
 }
