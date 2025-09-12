168508d8849a095f1218cfdc49f1650cfeeacd60
Magaranth Rajkumar <MRajkumar@stu.naperville203.org>
2025-09-12 12:14:10 -0500

    day 6
     huge ISSUE
    genius tnat figurred it out alas dylan was no help. He puted and shouted without helping much, rhte five guys remains in his bag waiting for me to eat it.

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -11,9 +11,24 @@ public class Sieve
         Scanner in = new Scanner(System.in);
         System.out.println("Compute primes up to which integer?");
         int n = in.nextInt();
-
-        // Your work goes here
-        . . .
+        java.util.HashSet<Integer> numbers = new java.util.HashSet<Integer>();
+        for (int i = 2; i <= n; i++) {
+            numbers.add(i);
+        }
+        for (int j = 2; j <= n; j++) {
+           if (j==2){
+            continue;
+           }
+           else{
+            if (j%2==0){
+                numbers.remove(j);
+            }
+            if (j%3==0){
+                numbers.remove(j);
+            }
+           }
+        }
+        System.out.println(numbers);
 
 
 
