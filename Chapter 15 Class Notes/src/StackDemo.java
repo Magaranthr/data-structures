a1bde0be0a4f17b7c7f3c9331365490932753d4c
Magaranthr <mrajkumar@stu.naperville203.org>
2025-09-15 13:11:53 -0500

    upd 6
    nothing tiday
    
    relad that us gmagaranth, eh is so supid. hje write bad bad thign bouab vs code
    
    anASSHOLE ******** n****

--- Chapter 15 Class Notes/src/StackDemo.java
+++ Chapter 15 Class Notes/src/StackDemo.java
@@ -5,9 +5,13 @@ import java.util.Stack;
  * must be undone in the opposite order in which they are first
  * issued.
 */
+// lets play a game where letters ar einput and every other letter is given in a separat eline them meshed together at the end without once reading the word. If the input was noipgegtefr 
+
 public class StackDemo
 {
     public static void main(String[] args)
     {
+        // Create an empty stack.
+        Stack<String> undoStack = new Stack<String>();
     }
 }
